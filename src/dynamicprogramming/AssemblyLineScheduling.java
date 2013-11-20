344848b6b733b757f578afe4caa26a36489daa7a
Alex Sun <alex.sun@oocl.com>
2013-11-20 16:51:43 +0800

    init

---
+++ src/dynamicprogramming/AssemblyLineScheduling.java
@@ -0,0 +1,37 @@
+package dynamicprogramming;
+
+public class AssemblyLineScheduling {
+	
+	private static final int[] a1 = new int[] {
+		7, 9, 3, 4, 8, 4
+	};
+	
+	private static final int[] a2 = new int[] {
+		8, 5, 6, 4, 5, 7
+	};
+	
+	private static final int[] t1 = new int[] {
+		2, 3, 1, 3, 4
+	};
+
+	private static final int[] t2 = new int[] {
+		2, 1, 2, 2, 1
+	};
+	
+	private static final int e1 = 2;
+	
+	private static final int e2 = 4;
+	
+	private static final int x1 = 3;
+	
+	private static final int x2 = 2;
+	
+	public static void fastestWay(int[] a1, int[] a2, int[] t1, int[] t2, int e1, int e2, int x1, int x2) {
+		
+	}
+	
+	public static void main(String... args) {
+		fastestWay(a1, a2, t1, t2, e1, e2, x1, x2);
+	}
+}
+
